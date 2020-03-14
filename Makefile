efc83f85a13a18a59ae050ae09867dbc3d7169a3
Cooper Lala <clala@il-f4.21-school.ru>
2020-03-14 16:37:50 +0300

    makefile works, example works

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
+INCS = -I./includes
 
+FLAGS = -Wall -Wextra -Werror
 
-SDL_INCS = -I./frameworks/SDL2.framework/Versions/A/Headers \
-		     -I./frameworks/SDL2_image.framework/Versions/A/Headers \
-			 -I./frameworks/SDL2_ttf.framework/Versions/A/Headers \
-			 -I./frameworks/SDL2_mixer.framework/Versions/A/Headers
+NAME = 1
+
+SDL_INCS = -I./frameworks/SDL2.framework/Versions/A/Headers
+
+		     #-I./frameworks/SDL2_image.framework/Versions/A/Headers \
+			 #-I./frameworks/SDL2_ttf.framework/Versions/A/Headers \
+			 #-I./frameworks/SDL2_mixer.framework/Versions/A/Headers
 FRAMEWORKS  = -F./frameworks \
 				-rpath ./frameworks \
-				-framework SDL2 -framework SDL2_ttf -framework SDL2_image -framework SDL2_mixer
-gcc foo.c - I (эти имена) (эти имена)
+				-framework SDL2 # -framework SDL2_ttf -framework SDL2_image -framework SDL2_mixer
+all:
+	gcc $(FLAGS) src/1.c $(INCS) $(SDL_INCS) $(FRAMEWORKS) -o $(NAME)
--- src/1.c
+++ src/1.c
@@ -1,38 +1,55 @@
 #include "../includes/1.h"
+#include <stdbool.h>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+const int SCREEN_START_POS_X = 100;
+const int SCREEN_START_POS_Y = 100;
 
-int main (int argc, char ** args) 
-{        
+int main (void) {
+    
+   
+
+
     if( SDL_Init( SDL_INIT_EVERYTHING ) != 0 )
     {
+        printf("error\n");
         return 1;
-    }
+    }    
+    SDL_Window* window = NULL;
+    window = SDL_CreateWindow("Hello, SDL 2!", 100, 
+                                100, SCREEN_WIDTH, SCREEN_HEIGHT, 
+                                SDL_WINDOW_SHOWN);
+    if (!window)
+        printf("window error\n");
 
     SDL_Surface* screen_surface = NULL;
 
-    SDL_Window* window = NULL;
-
-    window = SDL_CreateWindow("Hello, SDL 2!",SDL_WINDOWPOS_UNDEFINED, 
-    SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 
-    SDL_WINDOW_SHOWN);
+   
     
     if (window == NULL) {
         return 1;
     }
 
     screen_surface = SDL_GetWindowSurface(window);
+    
+        SDL_FillRect(screen_surface, NULL, SDL_MapRGB( screen_surface->format, 0, 255, 0));
+        SDL_UpdateWindowSurface(window);    
 
-    SDL_FillRect(screen_surface, NULL, SDL_MapRGB( screen_surface->format, 0, 255, 0));
+        //SDL_Delay(5000);
+         bool isquit = false;
+SDL_Event event;
+while (!isquit) {
+    if (SDL_PollEvent( & event)) {
+        if (event.type == SDL_QUIT) {
+            isquit = true;
+        }
+    }
+}
 
-    SDL_UpdateWindowSurface(window);
+        SDL_DestroyWindow(window);
 
-    SDL_Delay(2000);
-
-    SDL_DestroyWindow(window);
-
-    SDL_Quit();
+        SDL_Quit();
 
     return 0;
 }
