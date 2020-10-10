ae2c60dfc76a4d794e5585599e8eef60b5d17c66
live <live@128-72-44-82.broadband.corbina.ru>
2020-10-10 20:46:06 +0300

    исправил текстуры, но если скользить по стенке чет лагает, а так вроде все норм

--- src/pseudo_3d.c
+++ src/pseudo_3d.c
@@ -1,6 +1,6 @@
 #include "../includes/wolf3d.h"
 
-void	draw_column(t_wolf *wolf, SDL_Surface *surface, t_point point, t_distance dist, int size, int offsety)
+void	draw_column(t_wolf *wolf, SDL_Surface *surface, t_point point, t_distance dist, int size, float height)
 {
 	int color;
 
@@ -18,22 +18,20 @@ void	draw_column(t_wolf *wolf, SDL_Surface *surface, t_point point, t_distance d
 	else if (dist.tex == 'W')
 		color = 0x111111;
 	int y_start = point.y;
-	float height = size - point.y;
-	//printf("%d\n", point.y);
-	int i = 0;
-	       
+
+	int i = 0;      
 	while (point.y < size) // закрашиваем стенку по игреку или можно както сразу #строку# по вертикали закрсить ??
 	{
-		int y = point.y + W / 2 - size / 2;
 		if (dist.tex == 'W')
-			color = getpixel(wolf->sdl->textures, dist.offsetx, (int)(((float)i / height) * (CUBE - 2 * offsety)) + offsety);
+			color = getpixel(wolf->sdl->textures, dist.offsetx, (int)(((float)i / height) * 62));
 		else if (dist.tex == 'S')
-			color = getpixel(wolf->sdl->textures, dist.offsetx + CUBE, (int)(((float)i / height) * (CUBE - 2 * offsety)) + offsety);
+			color = getpixel(wolf->sdl->textures, dist.offsetx + CUBE, (int)(((float)i / height) * 62));
 		else if (dist.tex == 'N')
-			color = getpixel(wolf->sdl->textures, dist.offsetx + CUBE * 2, (int)(((float)i / height) * (CUBE - 2 * offsety)) + offsety);
+			color = getpixel(wolf->sdl->textures, dist.offsetx + CUBE * 2, (int)(((float)i / height) * 62));
 		else if (dist.tex == 'E')
-			color = getpixel(wolf->sdl->textures, dist.offsetx + CUBE * 3, (int)(((float)i / height) * (CUBE - 2 * offsety)) + offsety);
-		set_pixel(surface, point, color);
+			color = getpixel(wolf->sdl->textures, dist.offsetx + CUBE * 3, (int)(((float)i / height) * 62));
+		if (point.y > 0 && point.y < H)
+			set_pixel(surface, point, color);
 		point.y++;
 		i++;
 	}
@@ -48,48 +46,16 @@ void	pseudo_3d(t_wolf *wolf, t_player *player, SDL_Surface *surface)
     point.y = 0;
 	count_distance = W - 1; //номер луча с конца
 	int color = 255;
-	// data->color = mlx_get_color_value(data->mlx_p, 6560200);
-    // ft_printf("%f--mydis\n", player->distance[W / 2]);
 	while (point.x < W)
 	{
 		if (player->distance[count_distance].dist != 0) // проверка сталкивается ли луч с чем либо 
 		{
-			// y = (int)((1 - (data->player.distance[count_distance] / data->player.visibility)) * data->h);
-			point.y =  (CUBE * player->dist_to_canvas) /  player->distance[count_distance].dist;
-			// point.y = (int)(((2.0f * atan((1.0f / (2.0f * (float)player->distance[count_distance])))) / ((float)H / ((float)W / (float)(player->fov * 0.02)))) * (float)H);
-			
-			//что то альфа = 2 * арктан(1/2*дистанция)] находим колличество пикселей по игрек от серидины в обе стороны которые надо закрасить 
-			if (point.y > H) // затычка чтоб не крашилось при залете в стенку 
-				point.y = H;
-			int cash = H;
-			cash = (cash - point.y) / 2; // половина не закрашеной части по игрек (низ или верх) колличество пикселей
-			point.y = cash;
-			int y1 = point.y;
-			int offsety = 0;
-
-			
-			if (player->distance[count_distance].dist < player->dist_to_canvas)
-			{
-				offsety = CUBE - (int)(player->distance[count_distance].dist / player->dist_to_canvas) * CUBE;
-				printf("%d\n", offsety);
-			}
-			
-			
-			draw_column(wolf, surface, point, player->distance[count_distance], H - y1, offsety);
-			
-			//draw_column();
-			
-			//ft_printf("%d %d %d\n", point.y, y1, count_distance);
-			
+			point.y = (CUBE * player->dist_to_canvas) /  player->distance[count_distance].dist;
+			point.y = (H - point.y) / 2; // половина не закрашеной части по игрек (низ или верх) колличество пикселей
+			float height = H - point.y * 2;
+			draw_column(wolf, surface, point, player->distance[count_distance], H - point.y, height);			
 		}
 		count_distance--; // следующий луч
 		point.x++;;
 	}
 }
-
-// void set_pixel(SDL_Surface *surface, t_point point, Uint32 pixel)
-// {
-//   Uint32 *target_pixel = (Uint32 *)((Uint8 *)surface->pixels + point.y * surface->pitch
-//   	 + point.x * sizeof *target_pixel);
-//  	 *target_pixel = pixel;
-// }
